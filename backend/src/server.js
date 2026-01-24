666ec90c06fdc9763961f32682c5e60c0aebcec3
ake144 <tamiratgedi1@gmail.com>
2026-01-24 22:04:47 +0300

    fix: update CORS configuration and API base URL for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,8 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
 app.use(cors({
-    origin: true, // Reflects the request origin to allow all for development
-    credentials: true
+     origin: [process.env.FRONTEND_URL, 'http://localhost:3001', "https://hooraflix.esperanza.et/", 'https://hooraflix.com/'],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
